c1536f254cce0204028002a2b0880d777aeb9234
Michel Llorens <mllorens@dcc.uchile.cl>
2016-07-04 11:47:21 -0300

    First structures and dataset

---
+++ src/algorithms/Dinamic.java
@@ -0,0 +1,24 @@
+package algorithms;
+
+/**
+ * Class Dinamic
+ *
+ * @author Michel Llorens <mllorens@dcc.uchile.cl>
+ * @version 1.0, 04-07-2016
+ */
+public class Dinamic implements Hamiltonian{
+    @Override
+    public void calculate() {
+
+    }
+
+    @Override
+    public double getWeight() {
+        return 0;
+    }
+
+    @Override
+    public double getDuration() {
+        return 0;
+    }
+}
---
+++ src/algorithms/Hamiltonian.java
@@ -0,0 +1,13 @@
+package algorithms;
+
+/**
+ * Class Hamiltonian
+ *
+ * @author Michel Llorens <mllorens@dcc.uchile.cl>
+ * @version 1.0, 04-07-2016
+ */
+public interface Hamiltonian {
+    void calculate();
+    double getWeight();
+    double getDuration();
+}
